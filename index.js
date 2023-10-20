b0a02b27b37c2cd2eaf7fab5818282d75406e8a6
gahfaustino <tech.03@bpone.group>
2023-10-20 10:10:31 -0300

    remove tus from index.js

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ require('./config/db')
 
 // Import packages
 const express = require('express')
-const {Server, EVENTS} = require('@tus/server')
-const {FileStore} = require('@tus/file-store')
 const path = require('path')
 const bodyParser = require('body-parser')
 const busboyBodyParser = require('./helpers/body-parser')
@@ -16,7 +14,6 @@ const cors = require('cors')
 const validator = require('express-validator')
 const timeout = require('express-timeout-handler')
 const app = express()
-const uploadApp = express()
 const server = require('./config/server')
 const versions = ['v1']
 
@@ -24,47 +21,6 @@ require('./config/template').configure(app)
 require('./config/api_error')
 
 global._base = path.join(__dirname, '/')
-
-// // Add the authentication middleware.
-// app.use(authenticate({
-//   region: 'us-east-2',
-//   userPoolId: 'us-east-2_ln45PKOUh',
-//   tokenUse: ['id', 'access'],
-//   audience: ['https://dev-0w6h4qjzioi3gv0d.us.auth0.com/api/v2/']
-// }));
-
-
-
-const tuxServer = new Server({
-  path: '/',
-  datastore: new FileStore({
-    directory: './uploads'
-  }),
-  onUploadFinish: (req, res, upload) => {
-    console.log('UploadFinish', req.metaFields, upload.id);
-    try {
-      var fileData = fs.readFileSync('./uploads/'+upload.id)
-
-      console.log('UploadFinish', req.originalUrl)
-    } catch(err) {
-      console.log('Error Testing', err)
-    }
-  },
-  onIncomingRequest: (req, res) => {
-    // try {
-    //   var fileData = fs.readFileSync('.'+req.originalUrl)
-
-    console.log('IncomingRequest', req)
-    // //req.next()
-    // } catch(err) {
-    //   console.log('Error Testing', err)
-    // }
-  },
-})
-
-uploadApp.all('*', tuxServer.handle.bind(tuxServer))
-
-app.use('/upload', uploadApp)
 
 // Middlewares
 app.use(timeout.handler({
@@ -81,6 +37,7 @@ if (app.get('env') === 'development') {
     res.render(`${req.query.path}`, JSON.parse(req.query.data))
   })
 }
+
 app.use(busboyBodyParser())
 app.use(bodyParser.json({ limit: '42mb' }))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -104,11 +61,6 @@ app.get('/', (req, res) => {
   const { version } = require('./package.json')
   res.status(200).json({ version })
 })
-
-
-
-// // Add the authentication error handler.
-// app.use(authenticationError());
 
 // Global error handler
 app.use(errorHandler)
